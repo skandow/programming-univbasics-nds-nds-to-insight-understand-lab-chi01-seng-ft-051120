610dfeda1b9c5401a89d89480ea85294b7676724
Steven Kandow <kando1sr@gmail.com>
2020-03-09 13:47:11 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -4,8 +4,21 @@ require 'directors_database'
 
 def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
-  nil
+  require 'pp'
+  pp nds 
 end
 
 def print_first_directors_movie_titles
+  nds = directors_database
+  row_index = 0 
+  while row_index < nds.length do 
+    if nds[row_index][:name] = "Stephen Spielberg"
+      column_index = 0 
+      while column_index < nds[row_index][:movies].length do
+        puts nds[row_index][:movies][column_index]
+        column_index += 1 
+      end
+    end
+  row_index += 1
+  end 
 end
